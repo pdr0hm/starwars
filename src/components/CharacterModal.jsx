0e0a454f0ef44fb80be5a007fc7586220e1eab8e
Pedro H. Marques <pedro.henrique@sc.senai.br>
2022-05-09 11:28:06 -0300

    Ajustes modal e package.json

--- src/components/CharacterModal.jsx
+++ src/components/CharacterModal.jsx
@@ -1,24 +1,8 @@
 import Modal from "react-modal";
+import { useState } from "react";
 
 export function CharacterModal(character) {
   const [modalIsOpen, setIsOpen] = useState(false);
-
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-    },
-  };
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
-  }
 
   function closeModal() {
     setIsOpen(false);
